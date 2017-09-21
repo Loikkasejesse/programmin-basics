f8c86c7a05e8c07c554ce70dfac9d84cd0a3d27d
loikkasejesse <jesse.loikkanen@outlook.com>
2017-09-21 15:46:25 +0300

    jee

---
+++ conditional-statements/conditional-statement/conditional-statement/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace conditional_statement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma tulostaa positiivinen, negatiivinen tai nolla.");
+            Console.Write("Syötä luku:");
+            string userInput;
+            userInput = Console.ReadLine();
+
+            int evaluatedNumber;
+            int.TryParse(userInput, out evaluatedNumber);
+
+            if (evaluatedNumber < 0)
+            {
+                Console.WriteLine($"numero {evaluatedNumber} on negatiivinen");
+            }
+            else if(evaluatedNumber > 0)
+            {
+                Console.WriteLine($"numero {evaluatedNumber} on positiivinen");
+            }
+            else
+            {
+                Console.WriteLine($"numero {evaluatedNumber} on nolla");
+            }
+            Console.ReadKey();
+
+            
+
+        }
+    }
+}
